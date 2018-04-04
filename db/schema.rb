c024dcfd46227c4c58ef0b909dff9c907ce179b1
Kuniaki IGARASHI <igaiga@gmail.com>
2018-04-04 18:06:43 +0900

    Add point model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_04_04_091358) do
+  create_table "points", force: :cascade do |t|
+    t.integer "amount"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_points_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
