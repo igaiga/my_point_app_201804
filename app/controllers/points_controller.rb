7c9793b1c134cf4e43fe793893ec0bb034f629af
Kuniaki IGARASHI <igaiga@gmail.com>
2018-04-09 17:04:12 +0900

    Add /users/id/points/decrease.json

---
+++ app/controllers/points_controller.rb
@@ -0,0 +1,25 @@
+class PointsController < ApplicationController
+  def show
+    user_id = params.permit(:user_id, :format)[:user_id]
+    user = User.find(user_id) # TODO: token
+    respond_to do |format|
+      format.json { render json: { points: user.point }, status: :ok }
+    end
+  end
+
+  # /users/1/points/decrease.json?point=10
+  def decrease
+    user_id = params.permit(:user_id, :format)[:user_id]
+    point = params.permit(:point)[:point].to_i
+    raise ArgumentError.new("ポイントはプラスの値で頼むで") if point.nil? || point <= 0
+    remain_point = ActiveRecord::Base.transaction do
+      user = User.lock.find(user_id) # TODO: token
+      raise ArgumentError.new("ポイントが足りません") if point > user.point
+      user.decrease_point(point)
+      user.point
+    end
+    respond_to do |format|
+      format.json { render json: { points: remain_point }, status: :ok }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [] do
-    resources :points, only: [:index]
+    resource :points, only: [:show] do
+      get :decrease #TODO: POSTへ変更
+    end
   end
 end
