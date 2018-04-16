78f396644e2ce542e371b0b949302da0fac2d982
Kuniaki IGARASHI <igaiga@gmail.com>
2018-04-16 19:15:32 +0900

    Add example

--- bin/examples/get.rb
+++ bin/examples/get.rb
@@ -2,6 +2,7 @@ require "net/http"
 require "uri"
 require "json"
 uri = URI.parse("http://localhost:3000/users/1/points.json")
+#uri = URI.parse("http://192.168.x.x:3000/users/1/points.json")
 string = Net::HTTP.get(uri)
 hash = JSON.parse(string)
 p hash
--- bin/examples/post.rb
+++ bin/examples/post.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'uri'
 result =  Net::HTTP.post URI('http://localhost:3000/users/1/points/increase.json'), '{"point": 100}', 'Content-Type'=>'application/json'
+#result =  Net::HTTP.post URI('http://192.168.x.x:3000/users/1/points/increase.json'), '{"point": 100}', 'Content-Type'=>'application/json'
 #result =  Net::HTTP.post URI('http://localhost:3000/users/1/points/decrease.json'), '{"point": 1}', 'Content-Type'=>'application/json'
 p result.body
